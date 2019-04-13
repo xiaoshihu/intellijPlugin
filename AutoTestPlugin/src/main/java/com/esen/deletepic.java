e7a2aefe748ee7e2eb7f566bbb5cb470ad24f7ab
xiaoshihu <38485245+xiaoshihu@users.noreply.github.com>
2019-04-13 22:17:40 +0800

    Add a function to rename a screenshot and check screenshot already exists

--- AutoTestPlugin/src/main/java/com/esen/deletepic.java
+++ AutoTestPlugin/src/main/java/com/esen/deletepic.java
@@ -4,7 +4,6 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
